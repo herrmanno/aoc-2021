34cc2ef6316c1e2f7250dfee2efa39ae021acda9
Oliver Herrmann <herrmann@id-on.de>
2021-12-06 09:13:30 +0100

    Add day 6

--- src/Day6.hs
+++ src/Day6.hs
@@ -1,9 +1,28 @@
 module Day6 where
 
 import Puzzle ( Puzzle )
+import Data.List.Split (splitOn)
 
 part1 :: Puzzle
-part1 = undefined
+part1 = show . solve 80
 
 part2 :: Puzzle
-part2 = undefined
+part2 = show . solve 256
+
+solve d = sum . map ((+1) . childrenAfterDay d) . parseInput
+    where
+        childrenAfterDay d f = let birthDay = d + 8 - f in childrenAfterDaysToLive birthDay
+
+{-| calculates the summed up number of children a fish gives birth to, if he lives `d` days
+    after *after his birth*
+-}
+childrenAfterDaysToLive d = memo !! d
+    where
+        -- fish don't give birth in their first 8 days
+        memo = replicate 8 0 ++ map f [8..]
+        -- fish give first birth on their 9th day, then every other 7th day
+        f i = sum . map ((+1) . (memo !!)) . takeWhile (>=0) $ iterate (subtract 7) (i - 9)
+        --                ^      ^ the number of grandchildren
+        --                | add 1 to the number of grandchildren for the child fish itself
+
+parseInput = map read . splitOn ","
---
+++ test/Day6Spec.hs
@@ -0,0 +1,15 @@
+module Day6Spec (spec) where
+
+import Test.Hspec
+import Day6 (part1, part2)
+
+spec :: SpecWith ()
+spec = do
+    describe "day 6" $ do
+        it "should solve part 1" $ do
+            input <- readFile "input/6.txt"
+            part1 input `shouldBe` "351188"
+
+        it "should solve part 2" $ do
+            input <- readFile "input/6.txt"
+            part2 input `shouldBe` "1595779846729"
