36c7c8bccdee87a65eae59b79b019448c8d94848
Oliver Herrmann <herrmann@id-on.de>
2021-12-10 15:23:29 +0100

    Add day 10

--- src/Day10.hs
+++ src/Day10.hs
@@ -1,9 +1,46 @@
 module Day10 where
 
 import Puzzle ( Puzzle )
+import Data.Maybe (mapMaybe)
+import Data.List (sort)
 
 part1 :: Puzzle
-part1 = undefined
+part1 = show . sum . fmap (sumCorrupts . parseCommandLine) . lines
+    where
+        sumCorrupts Ok = 0
+        sumCorrupts (Incomplete _) = 0
+        sumCorrupts (Corrupt ')') = 3
+        sumCorrupts (Corrupt ']') = 57
+        sumCorrupts (Corrupt '}') = 1197
+        sumCorrupts (Corrupt '>') = 25137
+        sumCorrupts (Corrupt c) = error $ "Bad char in Corrupt :" <> show c
 
 part2 :: Puzzle
-part2 = undefined
+part2 = show . middle . sort . mapMaybe (sumIncomplete . parseCommandLine) . lines
+    where
+        middle xs = let i = (length xs - 1) `div` 2 in xs !! i
+        sumIncomplete Ok = Nothing
+        sumIncomplete (Corrupt _) = Nothing
+        sumIncomplete (Incomplete xs) =
+            let f acc '(' = 5 * acc + 1
+                f acc '[' = 5 * acc + 2
+                f acc '{' = 5 * acc + 3
+                f acc '<' = 5 * acc + 4
+                f acc c =  error $ "invalid char: " <> show c
+            in Just $ foldl f 0 xs
+
+data ParseResult = Ok | Corrupt Char | Incomplete String deriving Show
+
+parseCommandLine :: String -> ParseResult
+parseCommandLine s = go s [] where
+    go [] [] = Ok
+    go [] ys = Incomplete ys
+    go (x:xs) ys
+        | x `elem` "([{<"                      = go xs (x:ys)
+        | not (null ys) && head ys `matches` x = go xs (tail ys)
+        | otherwise                            = Corrupt x
+    matches '(' ')' = True
+    matches '[' ']' = True
+    matches '{' '}' = True
+    matches '<' '>' = True
+    matches _ _     = False
---
+++ test/Day10Spec.hs
@@ -0,0 +1,15 @@
+module Day10Spec (spec) where
+
+import Test.Hspec
+import Day10 (part1, part2)
+
+spec :: SpecWith ()
+spec = do
+    describe "day 10" $ do
+        it "should solve part 1" $ do
+            input <- readFile "input/10.txt"
+            part1 input `shouldBe` "321237"
+
+        it "should solve part 2" $ do
+            input <- readFile "input/10.txt"
+            part2 input `shouldBe` "2360030859"
