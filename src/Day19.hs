e15eed5dc2ac2205a32828d830b83e8f2294e819
Oliver Herrmann <herrmann@id-on.de>
2021-12-21 16:52:30 +0100

    Improve day 19

--- src/Day19.hs
+++ src/Day19.hs
@@ -4,10 +4,9 @@ import Puzzle ( Puzzle )
 import Util (readAllNums)
 import Data.List.Split (splitOn)
 import qualified Data.Set as S
-import Data.List (sort, group, permutations, elemIndex)
+import Data.Set (Set)
 import Control.Monad (guard)
-import Data.Maybe (listToMaybe, mapMaybe)
-import Debug.Trace
+import Data.Maybe (listToMaybe)
 
 part1 :: Puzzle Int
 part1 = S.size . fst . allBeacons . parseInput
@@ -15,11 +14,10 @@ part1 = S.size . fst . allBeacons . parseInput
 part2 :: Puzzle Int
 part2 = maxManhattan . snd . allBeacons . parseInput
 
-
-type Scanner = S.Set Coord3d
+type Scanner = Set Coord3d
 type Coord3d = [Int]
 
-allBeacons :: [S.Set [Int]] -> (S.Set [Int], [[Int]])
+allBeacons :: [Set [Int]] -> (Set [Int], [[Int]])
 allBeacons [] = (S.empty, [])
 allBeacons (x:xs) = go x xs 0 []
     where
@@ -27,7 +25,7 @@ allBeacons (x:xs) = go x xs 0 []
         go x (y:ys) i ds = case matchScanners x y of
             (Just (x', d)) -> go x' ys 0 (d:ds)
             Nothing
-                | i == length (y:ys) -> error "Can not find matching beacons"
+                | i == 2 * length (y:ys) -> error "Can not find matching beacons"
                 | otherwise -> go x (ys ++ [y]) (i + 1) ds
 
 maxManhattan :: (Num a, Ord a) => [[a]] -> a
@@ -36,43 +34,48 @@ maxManhattan xs = maximum $ do
     b@[x',y',z'] <- xs
     return $ sum $ zipWith (\x y -> abs (x - y)) a b
 
-matchScanners :: S.Set [Int] -> S.Set [Int] -> Maybe (S.Set [Int], [Int])
+matchScanners :: Set [Int] -> Set [Int] -> Maybe (Set [Int], [Int])
 matchScanners as bs = listToMaybe $ do
-    -- let aDistances = allDiffs (S.toList as)
-    -- let bDistances = allDiffs (S.toList bs)
-    -- guard $ S.size (aDistances `S.intersection` bDistances) >= 80
-    a <- S.toList as
-    bs' <- moveToAllDirections bs
-    bs'' <- moveToAllPermutations bs'
+    let aDistances = allDiffs (S.toList as)
+    let bDistances = allDiffs (S.toList bs)
+    guard $ S.size (aDistances `S.intersection` bDistances) >= sum [1..12]
+    a <- drop 11 $ S.toList as
+    bs'' <- moveToAllRotations bs
     (bs''',diff) <- moveSetToMatch a bs''
-    let common = as `S.intersection` bs'''
-    guard $ S.size common >= 12
+    guard $ commonSizeAtLeast 12 (S.toList bs''')  as
     return (as `S.union` bs''', diff)
+
+commonSizeAtLeast 0 _ bs = True
+commonSizeAtLeast _ [] bs = False
+commonSizeAtLeast i (x:xs) bs = let i' = if x `S.member` bs then i - 1 else i
+                                in i' `seq` commonSizeAtLeast i' xs bs
 
 allDiffs xs = S.fromList $ do
     (i,a) <- zip [1..] xs
     b <- drop i xs
     return $ sum $ zipWith (\x y -> abs (x - y)) a b
 
-moveToAllDirections :: S.Set [Int] -> [S.Set [Int]]
-moveToAllDirections xs = fmap (\f -> S.map (zipWith ($) f) xs) directions
+moveToAllRotations :: Set [Int] -> [Set [Int]]
+moveToAllRotations xs = fmap (\arr -> S.map (rotate arr) xs) rotations
     where
-        directions = [ [a,b,c] | a <- [id,negate], b <- [id,negate], c <- [id,negate] ]
-        -- directions = [ [id,id,id], [id,negate,negate]
-        --              , [negate,negate,id], [negate,id,negate]
-        --              ]
+        rotate xyz xs = fmap (\i -> signum i * xs !! (abs i  - 1)) xyz
+        (x,y,z) = (1,2,3)
+        rotations =
+            [ [x,y,z], [x,-z,y], [x,-y,-z], [x,z,-y]
+            , [-x,-y,z], [-x,-z,-y], [-x,y,-z], [-x,z,y]
 
-moveToAllPermutations :: Ord c => S.Set [c] -> [S.Set [c]]
-moveToAllPermutations xs = fmap (\p -> S.map (rotate p) xs) ps
-    where
-        ps = permutations [0,1,2]
-        rotate p xs = map (xs !!) p
+            , [y,-z,-x], [y,x,-z], [y,z,x], [y,-x,z]
+            , [-y,z,-x], [-y,x,z], [-y,-z,x], [-y,-x,-z]
+
+            , [z,y,-x], [z,x,y], [z,-y,x], [z,-x,-y]
+            , [-z,-y,-x], [-z,x,-y], [-z,y,x], [-z,-x,y]
+            ]
 
 -- | Move all elements in a set, so that one of those elements equals `a` 
 --   Performs this manipulation for each element of the set
-moveSetToMatch:: (Show b, Eq b, Num b, Ord b) => [b] -> S.Set [b] -> [(S.Set [b], [b])]
+moveSetToMatch:: [Int] -> Set [Int] -> [(Set [Int], [Int])]
 moveSetToMatch a bs = do
-    b <- S.toList bs
+    b <- drop 11 $ S.toList bs
     let diffs = zipWith (-) a b
     let bs' = S.map (zipWith (+) diffs) bs
     return (bs',diffs)
